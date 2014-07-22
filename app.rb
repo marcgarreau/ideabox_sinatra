cd62ab60cdae19db18c404d18b1bc9a8277d16cc
Marc Garreau <marcdgarreau@gmail.com>
2014-07-21 19:53:45 -0600

    I1: recording ideas

--- app.rb
+++ app.rb
@@ -1,6 +1,22 @@
+require './idea'
+
 class IdeaBoxApp < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  not_found do
+    erb :error
+  end
+
   get '/' do
-    "Hello, World!"
+    erb :index
+  end
+
+  post '/' do
+    idea = Idea.new
+    idea.save
+    "Creating an IDEA!"
   end
 
   run! if app_file == $0
