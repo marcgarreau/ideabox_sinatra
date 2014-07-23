86c81b547f4f95fa6a462b162f224629f903d3a3
Marc Garreau <marcdgarreau@gmail.com>
2014-07-23 16:07:08 -0600

    omg everything changed. forgot to commit. adds images and styling.

--- lib/app.rb
+++ lib/app.rb
@@ -1,15 +1,27 @@
+require 'pry'
 require 'idea_box'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sass'
 
 class IdeaBoxApp < Sinatra::Base
+  set :root, 'lib/app'
   get('/styles.css'){ scss :styles }
   set :method_override, true
-  set :root, 'lib/app'
+  set :public_folder, 'public'
 
   configure :development do
     register Sinatra::Reloader
+  end
+
+  configure do
+    enable :sessions
+    set :username, 'marc'
+    set :password, 'omg'
+  end
+
+  get '/login' do
+    erb :login
   end
 
   get '/:id/edit' do |id|
@@ -17,8 +29,20 @@ class IdeaBoxApp < Sinatra::Base
     erb :edit, locals: {idea: idea}
   end
 
+  get '/filter' do
+    erb :index, locals: {
+        ideas: IdeaStore.all.select {|idea| idea.tags.include? params[:selected_tag]},
+        idea: Idea.new(params),
+        tags: IdeaStore.all.flat_map {|idea| idea.tags}.uniq.sort
+      }
+  end
+
   get '/' do
-    erb :index, locals: {ideas: IdeaStore.all, idea: Idea.new(params)}
+    erb :index, locals: {
+        ideas: IdeaStore.all.sort,
+        idea: Idea.new(params),
+        tags: IdeaStore.all.flat_map {|idea| idea.tags}.uniq.sort
+      }
   end
 
   put '/:id' do |id|
@@ -26,9 +50,35 @@ class IdeaBoxApp < Sinatra::Base
     redirect '/'
   end
 
+  post '/login' do
+    if params[:username] == settings.username && params[:password] == settings.password
+      session[:admin] = true
+      redirect to('/')
+    else
+      erb :login
+    end
+  end
+
   post '/:id/like' do |id|
     idea = IdeaStore.find(id.to_i)
     idea.like!
+    IdeaStore.update(id.to_i, idea.to_h)
+    redirect '/'
+  end
+
+  post '/:id/add_tag' do |id|
+    idea = IdeaStore.find(id.to_i)
+    idea.add_a_tag(params[:idea])
+    IdeaStore.update(id.to_i, idea.to_h)
+    redirect '/'
+  end
+
+  post '/:id/upload' do |id|
+    File.open('public/images/' + params['myfile'][:filename], "w") do |f|
+      f.write(params['myfile'][:tempfile].read)
+    end
+    idea = IdeaStore.find(id.to_i)
+    idea.add_upload(params[:myfile][:filename])
     IdeaStore.update(id.to_i, idea.to_h)
     redirect '/'
   end
--- lib/idea_box/idea.rb
+++ lib/idea_box/idea.rb
@@ -4,16 +4,39 @@ require 'yaml/store'
 class Idea
   include Comparable
   attr_reader :title, :description, :rank, :id
+  attr_accessor :tags, :myfile
 
   def initialize(attributes = {})
-    @title = attributes["title"]
+    @title       = attributes["title"]
     @description = attributes["description"]
-    @rank = attributes["rank"] || 0
-    @id = attributes["id"]
+    @rank        = attributes["rank"] || 0
+    @id          = attributes["id"]
+    @tags        = [attributes["tags"]].flatten
+    @myfile      = attributes["myfile"]
+  end
+
+  def to_h
+    {
+      "title"       => title,
+      "description" => description,
+      "rank"        => rank,
+      "id"          => id,
+      "tags"        => tags,
+      "myfile"      => myfile
+    }
   end
 
   def like!
     @rank += 1
+  end
+
+  def add_a_tag(data)
+    @tags << data[:tags]
+  end
+
+  def add_upload(filename)
+    @myfile = ""
+    @myfile << filename
   end
 
   def <=>(other)
